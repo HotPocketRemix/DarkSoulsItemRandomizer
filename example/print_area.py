a75d2976337776e3c6f4148e01f48e1f5b8f6c0c
massivelivefun <matthewlancefuller@gmail.com>
2020-03-27 20:28:31 -0700

    Add start of example directory

---
+++ example/print_area.py
@@ -0,0 +1,7 @@
+from proof.src.area_node import AreaNode
+
+undead_asylum = AreaNode("Undead Asylum", ['firelink_shrine'], {})
+
+firelink_shrine = AreaNode("Firelink Shrine", [], {})
+
+print(undead_asylum.borders[0])
