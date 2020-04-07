a2fe2e0fa06c7b213d3b52cb2e65ffb02b4300a7
DCiliberto <dominicciliberto@gmail.com>
2020-04-06 20:22:05 -0700

    unit tests for key_items_setup + some assorted exception tests

--- tests/test_item_lot_param.py
+++ tests/test_item_lot_param.py
@@ -1,5 +1,9 @@
 import item_lot_param
 import unittest
 
+class init_test(unittest.TestCase):
+    def runTest(self):
+        self.assertRaises(ValueError, item_lot_param(9,9,9,9,9,9,9))
+
 if __name__ == "__main__":
     unittest.main()
